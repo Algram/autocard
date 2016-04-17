0997df1c3a61689cf1e602637593333f55eab023
Algram <aliasgram@gmail.com>
2016-04-17 17:58:39 +0200

    Added Sistrix and GTmetrix

--- controller/mainController.js
+++ controller/mainController.js
@@ -1,38 +1,98 @@
 var request = require('request');
 var api = require('../config/api');
 
+/*getGTmetrix('http://google.de', function(res) {
+  console.log(res);
+});*/
 
-request({
+getSistrix('http://witt-weiden.de',{
+  method: 'links.overview', //domain.sichtbarkeitsindex
+  mobile: false
+}, function(res) {
+  console.log(res);
+});
+
+function getSistrix(url, options, cb) {
+  var qs = {
+    domain: url,
+    format: 'json',
+    api_key: api.sistrix.key
+  };
+
+  if (options.mobile) {
+    qs.mobile = options.mobile;
+  }
+
+  request({
+    url: 'http://api.sistrix.net/' + options.method,
+    method: 'GET',
+    qs: qs
+  })
+  .on('response', function(response) {
+    // Add error handling here
+    // console.log(response.statusCode);
+  })
+  .on('data', function(dataRaw) {
+    var data = JSON.parse(dataRaw);
+
+    switch (options.method) {
+      case 'domain.sichtbarkeitsindex':
+        cb(data.answer[0].sichtbarkeitsindex[0].value);
+        break;
+      case 'links.overview':
+        cb(data.answer[0].domains[0].num);
+        break;
+    }
+
+  });
+}
+
+function getGTmetrix(url, cb) {
+  request({
     url: 'https://gtmetrix.com/api/0.1/test',
     method: 'POST',
     form: {
-      url: 'https://www.google.com'
+      url: url
     },
     auth: {
-      user: api.user,
-      pass: api.pass
+      user: api.gtmetrix.user,
+      pass: api.gtmetrix.pass
     }
   })
-  .on('response', function(response, body) {
-    console.log(response.statusCode);
-    console.log(body);
+  .on('response', function(response) {
+    // Add error handling here
   })
   .on('data', function(data) {
     var poll_state_url = JSON.parse(data.toString()).poll_state_url;
+    loop();
 
-    setTimeout(function() {
+    var finished = false;
+    function loop() {
+
       request({
-          url: poll_state_url,
-          method: 'GET',
-          auth: {
-            user: api.user,
-            pass: api.pass
-          }
-        })
-        .on('data', function(data) {
-          var poll_state_url = JSON.parse(data.toString());
-          console.log(poll_state_url);
-        });
-    }, 5000);
+        url: poll_state_url,
+        method: 'GET',
+        auth: {
+          user: api.gtmetrix.user,
+          pass: api.gtmetrix.pass
+        }
+      })
+      .on('data', function(dataRaw) {
+        var data = JSON.parse(dataRaw);
 
+        if (data.state === 'completed') {
+          finished =  true;
+
+          cb({
+            pagespeed_score: data.results.pagespeed_score,
+            yslow_score: data.results.yslow_score
+          });
+        }
+
+        if (!finished) {
+          setTimeout(loop, 2000);
+        }
+      });
+    }
   });
+}
