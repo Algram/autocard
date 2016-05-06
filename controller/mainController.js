145649a8096470b51a7ff985082f6e2eb4845ccd
Algram <aliasgram@gmail.com>
2016-05-06 19:33:28 +0200

    Added urlcrawlerrorscounts request

--- controller/mainController.js
+++ controller/mainController.js
@@ -19,10 +19,13 @@ function getGWT(url, cb) {
     // See https://cloud.google.com/compute/docs/authentication for more information.
     if (authClient.createScopedRequired && authClient.createScopedRequired()) {
       // Scopes can be specified either as an array or as a single, space-delimited string.
-      authClient = authClient.createScoped(['https://www.googleapis.com/auth/webmasters.readonly']);
+      console.log('abe');
+      authClient = authClient.createScoped(['https://www.googleapis.com/auth/webmasters']);
     }
 
-
+    gwt.urlcrawlerrorscounts.query({ auth: authClient, siteUrl: 'http://www.witt-weiden.de'}, function(err, data) {
+      console.log(err, data);
+    });
   });
 }
 
